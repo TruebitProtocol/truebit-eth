50b13ee12604e872cef4d8c17e9072257d8f9be1
Truja <11977074+teutsch@users.noreply.github.com>
2020-08-30 10:10:15 -0600

    fixes for scrypt

--- wasm-ports/samples/scrypt/compile.sh
+++ wasm-ports/samples/scrypt/compile.sh
@@ -5,6 +5,6 @@ em++ -O2 -I $EMSCRIPTEN/system/include -c -std=c++11 scrypt.cpp
 em++ -o scrypt.js scrypthash.o scrypt.o -lcrypto -lssl
 
 node ~/emscripten-module-wrapper/prepare.js scrypt.js --file input.data --file output.data --run --debug --out=dist --memory-size=20 --metering=5000 --upload-ipfs --limit-stack
-cp dist/globals.wasm task.wasm
+cp dist/stacklimit.wasm task.wasm
 cp dist/info.json .
 solc --overwrite --bin --abi --optimize contract.sol -o build
